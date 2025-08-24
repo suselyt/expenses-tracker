c9bd6148e5d70351d963a21d503f2685b3962d52
Susely <susely.dev@gmail.com>
2025-08-23 21:42:37 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount': amount, 'category': category})
+    
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
+def total_expenses(expenses):
+    pass
+    
+
+
+expenses = []
